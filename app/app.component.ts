7a7959ba728fb52b5d0399fecfb78be81e24ebef
sholomka <sholomka@gmail.com>
2016-09-15 10:41:22 +0300

    aaa

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,20 @@
 import { Component } from '@angular/core';
-import { TodoListComponent } from './todo/todo-list.component';
-import { TodoFormComponent } from './todo/todo-form.component';
-import { Todo } from './todo/todo';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import {TodoService} from "./shared/todo.service";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.component.html',
     styleUrls: ['./app/app.component.css'],
-    directives: [TodoListComponent, TodoFormComponent]
+    directives: [TodoListComponent, TodoFormComponent],
+    providers: [TodoService]
 })
+
 export class AppComponent {
     title: string;
-    todos: Todo[];
 
     constructor() {
         this.title = 'New winner';
-        this.todos = []
-    }
-
-    onTodoAdded(todo: Todo) {
-        this.todos.push(todo);
     }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import {TodoListComponent} from "./todo/todo-list.component";
-import {TodoItem} from "./todo/todo-item.component";
-import {TodoFormComponent} from "./todo/todo-form.component";
+import {TodoListComponent} from "./components/todo-list/todo-list.component";
+import {TodoItem} from "./components/todo-item/todo-item.component";
+import {TodoFormComponent} from "./components/todo-form/todo-form.component";
 
 @NgModule({
   imports: [ BrowserModule ],
